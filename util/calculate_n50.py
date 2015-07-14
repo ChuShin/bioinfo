5d74b15e30c47f1f7c585f03d0a9d2b7f3a98abb
Kevin Koh <kkoh@Blackpearl.local>
2015-07-13 21:27:26 -0700

    add docs

--- util/calculate_n50.py
+++ util/calculate_n50.py
@@ -42,7 +42,9 @@ def read_sizes_from_fasta_file(filename):
 
     return sizes, sum_length
 
+
 [sizes, sum_length] = read_sizes_from_fasta_file(args.fasta_file)
+
 num_seq = "{:,}".format(len(sizes))
 max_len = "{:,}".format(sizes[0])
 min_len = "{:,}".format(sizes[-1])
