c041c0a00cd915de3f62c55037d96e5e7f4ce94f
ChuShin <ChuShin.Koh@gmail.com>
2017-04-03 16:20:50 -0700

    add min_score cutoff

--- util/barcoded_bam.py
+++ util/barcoded_bam.py
@@ -4,13 +4,14 @@ import argparse
 import pysam
 import sys
 
-def read_bam(filename):
+def read_bam(filename, min_score):
     samfile = pysam.AlignmentFile(filename, 'rb')
     for read in samfile.fetch():
         read_tags = dict(read.tags)
-        if read_tags['MI']:
-            print '%s\t%s\t%d\t%s' %(read.query_name, read.reference_name,
-                                     read.reference_start, read_tags['MI'])
+        if 'MI' in read_tags and read_tags['AS'] >= min_score:
+            print '%s\t%s\t%d\t%s\t%s' %(read.query_name, read.reference_name,
+                                         read.reference_start, read_tags['MI'],
+                                         read_tags['BX'])
     samfile.close()
 
 
@@ -18,9 +19,10 @@ def main():
     parser = argparse.ArgumentParser(
         description='Parser for barcoded BAM')
     parser.add_argument('-mi', '--molecule_only', type=bool, default=1)
+    parser.add_argument('-ms', '--min_score', type=int, default=30)
     parser.add_argument('bam_filename', type=str)
     args = parser.parse_args()
-    read_bam(args.bam_filename)
+    read_bam(args.bam_filename, args.min_score)
 
 
 if __name__ == '__main__':
