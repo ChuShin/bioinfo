da4e97d57d704eca71eaa8b5ad331533c43a1686
ChuShin <ChuShin.Koh@gmail.com>
2017-04-03 16:01:18 -0700

    parse barcoded bam file for global molecule id.

--- util/barcoded_bam.py
+++ util/barcoded_bam.py
@@ -5,13 +5,13 @@ import pysam
 import sys
 
 def read_bam(filename):
-    samfile = pysam.AlignmentFile(in_file, 'rb')
+    samfile = pysam.AlignmentFile(filename, 'rb')
     for read in samfile.fetch():
-     if read.is_proper_pair:
-             pairedreads.write(read)
-
-pairedreads.close()
-samfile.close()
+        read_tags = dict(read.tags)
+        if read_tags['MI']:
+            print '%s\t%s\t%d\t%s' %(read.query_name, read.reference_name,
+                                     read.reference_start, read_tags['MI'])
+    samfile.close()
 
 
 def main():
@@ -19,6 +19,8 @@ def main():
         description='Parser for barcoded BAM')
     parser.add_argument('-mi', '--molecule_only', type=boolean, default=TRUE)
     parser.add_argument('bam_filename', type=str)
+    args = parser.parse_args()
+    read_bam(args.bam_filename)
 
 
 if __name__ == '__main__':
