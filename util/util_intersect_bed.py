4c8092f2a539b391cd8b1e8279a87547b30a5912
ChuShin <ChuShin.Koh@gmail.com>
2017-06-23 23:48:04 -0600

    read bed file.

--- util/util_intersect_bed.py
+++ util/util_intersect_bed.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 from collections import defaultdict
 
+
 def read_bed:
     with open(beda, 'r') as bedfile:
         for bed in bedfile:
@@ -15,14 +16,11 @@ def read_bed:
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Given a read alignment file and a base position on reads, "
-            "produce a GFF file of where each base pos is located on the reference.")
-    parser.add_argument('-b', '--base_pos_info_file', type=str)
-    parser.add_argument('-o', '--output_file', type=str)
-    parser.add_argument('input_sam_file', type=str)
+        description="")
+    parser.add_argument('-a', '--abed_file', type=str)
+    parser.add_argument('-b', '--bbed_file', type=str)
     args = parser.parse_args()
-    queries = load_pos_in_bed_file(args.base_pos_info_file)
-    parse_sam(args.input_sam_file, args.output_file, queries)
+    read_bed(args.abed_file)
 
 
 if __name__ == "__main__":
