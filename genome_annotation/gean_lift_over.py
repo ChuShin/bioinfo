4bb12dbf7fd80987e5b67004f1ce16b1861a401a
ChuShin <ChuShin.Koh@gmail.com>
2017-07-11 23:47:03 -0700

    add documentation.

--- genome_annotation/gean_lift_over.py
+++ genome_annotation/gean_lift_over.py
@@ -7,3 +7,19 @@ from collections import defaultdict
 
 """gean_liftover.py:
 Given an input GFF file ."""
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Convert coordinates of GFF file onto new REFs '
+                    'according to an AGP file. ')
+    parser.add_argument('gff_filename', type=str)
+    parser.add_argument('agp_filename', type=str)
+    args = parser.parse_args()
+    read_gff(args.gff_filename)
+
+if __name__ == '__main__':
+    main()
