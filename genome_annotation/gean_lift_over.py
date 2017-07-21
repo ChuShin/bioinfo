60f1f8c29c4e4237929784debd04478d627a9f8c
ChuShin <ChuShin.Koh@gmail.com>
2017-07-21 06:02:48 +0200

    implemented reverse lookup and added test data.

--- genome_annotation/gean_lift_over.py
+++ genome_annotation/gean_lift_over.py
@@ -18,8 +18,12 @@ def read_agp_file(filename):
              component_type, component_id, component_beg,
              component_end, strand] = line.strip().split('\t')
             if component_type == 'W':
-                chrs[object][object_beg] = [component_id, component_beg,
-                                            component_end, strand]
+                chrs[object][object_end] = {'object_beg': int(object_beg),
+                                            'object_end': int(object_end),
+                                            'component_id': component_id,
+                                            'component_beg' : int(component_beg),
+                                            'component_end' :int(component_end),
+                                            'strand' : strand}
     return chrs
 
 # to-do: should create a separate class for bed file type
@@ -27,16 +31,37 @@ def lift_over(filename, chrs):
     with open(filename, 'r') as bed:
         for line in bed:
             try:
-                [chr, start, end, feature, score, strand] = \
+                [chr, start, end, feature] = \
                     line.strip().split('\t')
-
-
+                for position in chrs[chr]:
+                    if position >= start:
+                        component = chrs[chr][position]
+                        new_start, new_end = lookup(int(start), int(end),
+                                                    component)
+                        print '%s\t%d\t%d\t%s' %(component['component_id'],
+                                                 new_start, new_end, feature)
+                        break
             except ValueError:
-                print >> sys.stderr, 'Invalid 6-col bed file, please check format.'
+                print >> sys.stderr, 'Invalid 4-col bed file, please check format.'
                 sys.exit(1)
             except Exception, e:
                 print >> sys.stderr, 'Exception: %s' %str(e)
                 sys.exit(1)
+
+def lookup(start, end, component):
+    if component['object_end'] < end:
+        print >> sys.stderr, 'Feature [%d, %d] crossed an object boundary %s' \
+                             %(start, end, component)
+    new_start = start - component['object_beg']
+    new_end = end - component['object_beg']
+    if component['strand'] == '+':
+        new_start = new_start + component['component_beg']
+        new_end = new_end + component['component_beg']
+    elif component['strand'] == '-':
+        tmp_pos = new_start
+        new_start = component['object_end'] - new_end
+        new_end = component['object_end'] - tmp_pos
+    return new_start, new_end
 
 def main():
     parser = argparse.ArgumentParser(
