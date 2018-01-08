c4048f994a1cbffc04e40d37f529f304ac12ca20
ChuShin <ChuShin.Koh@gmail.com>
2018-01-07 16:56:34 -0800

    order and orientate scaffolds by orthologs

--- util/util_order_scaffolds.py
+++ util/util_order_scaffolds.py
@@ -24,19 +24,30 @@ def get_scaffolds_in_chrs(input_file):
 
 def filter_promicuous_scaffolds(scaffolds):
     chromosomes = defaultdict(lambda: defaultdict(list))
-#    results = defaultdict(lambda: defaultdict(int))
+
     for sc in scaffolds:
-        for chr,coords in scaffolds[sc].items():
-            print sc,chr,coords
-            sc_chr_coords = []
-            sc_coords = []
-            sc_orient = '+'
-            for coord in coords:
-                sc_chr_coords.append(coord[0])
-                sc_coords.append(coord[1])
-            if len(coords) > 1 and sc_coords[0] > sc_coords[-1]:
-                sc_orient = '-'
-            print sc,chr,len(coords), sc_orient, median(sc_chr_coords)
+        chr_sort = sorted(scaffolds[sc].items(), key=lambda x: len(x[1]),reverse=True)
+        chr = chr_sort[0][0]
+        coords = chr_sort[0][1]
+        num_best = len(coords)
+
+        if(len(chr_sort) > 1):
+            num_runner = len(chr_sort[1][1])
+            if float(num_runner)/num_best > 0.6:
+                #debug message here
+                continue
+#        print sc,chr,coords
+        sc_chr_coords = []
+        sc_coords = []
+        sc_orient = '+'
+        for coord in coords:
+            sc_chr_coords.append(coord[0])
+            sc_coords.append(coord[1])
+        sc_anchor_pos = median(sc_chr_coords)
+        if len(coords) > 1 and sc_coords[0] > sc_coords[-1]:
+            sc_orient = '-'
+        chromosomes[chr][sc].append([sc_anchor_pos, num_best,sc_orient])
+    return chromosomes
 
 
 def main():
@@ -46,6 +57,13 @@ def main():
     args = parser.parse_args()
     scaffolds = get_scaffolds_in_chrs(args.bed_filename)
     chromosomes = filter_promicuous_scaffolds(scaffolds)
+    for chr in chromosomes:
+        sc_sort = sorted(chromosomes[chr].items(), key=lambda x: x[1])
+#        print sc_sort
+        for sc_order in sc_sort:
+            sc = sc_order[0]
+            sc_info = chromosomes[chr][sc]
+            print "%s\t%s\t%f\t%s\t%s" %(chr, sc,sc_info[0][0],sc_info[0][1],sc_info[0][2])
 
 if __name__ == '__main__':
     main()
