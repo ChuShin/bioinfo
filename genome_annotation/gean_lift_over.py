5daa1a072bf32ce04efb6d303f553fd960ed9023
ChuShin <ChuShin.Koh@gmail.com>
2018-05-10 13:50:03 -0700

    add GFF conversion and argument.

--- genome_annotation/gean_lift_over.py
+++ genome_annotation/gean_lift_over.py
@@ -11,9 +11,9 @@ Given an AGP file, convert coordinates in an input GFF/BED file between object
 and component coordinates."""
 
 
-# to-do: should create a separate class for agp file type
+# to-do: create a separate class for agp file type
 def read_agp_file(filename):
-    chrs = defaultdict(lambda : defaultdict(lambda: list))
+    chrs = defaultdict(lambda : defaultdict(lambda: dict))
     with open(filename, 'r') as agp:
         for line in agp:
             if not line.startswith('#'):
@@ -29,8 +29,9 @@ def read_agp_file(filename):
                                                 'strand' : strand}
     return chrs
 
-# to-do: should create a separate class for bed file type
-def lift_over(filename, chrs):
+
+# to-do: create a separate class for bed file type
+def bed_lift_over(filename, chrs):
     with open(filename, 'r') as bed:
         for line in bed:
             try:
@@ -51,6 +52,35 @@ def lift_over(filename, chrs):
             except Exception, e:
                 print >> sys.stderr, 'Exception: %s' %str(e)
                 sys.exit(1)
+
+
+# to-do: create a separate class for bed file type
+def gff_lift_over(filename, chrs):
+    with open(filename, 'r') as gff:
+        for line in gff:
+            try:
+                if line.startswith('#'):
+                    print line
+                else:
+                    [chr, source, feature_type, start, end, score, strand,
+                     frame, feature_name] = line.strip().split('\t')
+                    for position in sorted(chrs[chr]):
+                        if position >= int(start):
+                            component = chrs[chr][position]
+                            new_start, new_end, new_strand = \
+                                lookup(int(start), int(end), strand, component)
+                            print '%s\t%d\t%d\t%s\t%s\t%s' %(
+                                component['component_id'], source,
+                                feature_type, new_start, new_end, score,
+                                strand, frame, feature_name)
+                            break
+            except ValueError:
+                print >> sys.stderr, 'Invalid 9-col GFF file format.'
+                sys.exit(1)
+            except Exception, e:
+                print >> sys.stderr, 'Exception: %s' %str(e)
+                sys.exit(1)
+
 
 def lookup(start, end, strand, component):
     if component['object_end'] < end:
@@ -81,13 +111,21 @@ def main():
     parser = argparse.ArgumentParser(
         description='Given an AGP file, convert coordinates in an input '
                     'GFF/BED file between object and component coordinates')
-#    parser.add_argument('-gff', '--gff_filename', type=str)
+    parser.add_argument('-gff', '--gff_filename', type=str)
     parser.add_argument('-bed', '--bed_filename', type=str)
     parser.add_argument('-c', '--to_component', type=bool, default=0)
     parser.add_argument('agp_filename', type=str)
     args = parser.parse_args()
     chrs = read_agp_file(args.agp_filename)
-    lift_over(args.bed_filename,chrs)
+    if args.gff_filename:
+        gff_lift_over(args.gff_filename,chrs)
+    elif args.bed_filename:
+        bed_lift_over(args.bed_filename,chrs)
+    else:
+        print >> sys.stderr, 'Please provide either a GFF or BED file'
+        sys.exit(1)
+
+
 
 if __name__ == '__main__':
     main()
