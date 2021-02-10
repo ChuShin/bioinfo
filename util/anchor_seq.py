2c4e3926f7b34469a24ca23c96aa7b647b3e14e7
chk512 <kevin.koh@gifs.ca>
2021-02-09 20:27:40 -0800

    add anchor_seq script.

--- util/anchor_seq.py
+++ util/anchor_seq.py
@@ -41,24 +41,14 @@ def load_coords(filename):
     return alns
 
 
-def load_query_assm_lengths(filename):
-    querys = defaultdict(lambda: defaultdict(int))
+def load_seq_lengths(filename):
+    seqs = defaultdict(lambda: defaultdict(int))
     with open(filename, 'r') as infile:
         for line in infile:
-            key, val = line.strip().split('\t')
-            query_name, c_name = key.split('_')
-            querys[query_name][key] = float(val)
-    return querys
+            seq, val = line.strip().split('\t')
 
-
-def load_ref_assm_lengths(filename):
-    refs = defaultdict(lambda: defaultdict(int))
-    with open(filename, 'r') as infile:
-        for line in infile:
-            key, val = line.strip().split('\t')
-            refs[key] = float(val)
-    return refs
-
+            seqs[seq] = int(val)
+    return seqs
 
 def summarize_ref_simple(alns, refs, min_pid, min_aln_len):
     """
@@ -99,47 +89,8 @@ def summarize_simple(alns, querys, min_pid, min_aln_len):
 #                print "\t\t", ranges
                 total_bases = sum(e - s + 1 for s, e in ranges)
                 print "%s\t%s\t%s\t%d/%d\t%2.1f" \
-                      % (query, ref, contig, total_bases, querys[query][contig],
-                         100 * total_bases / querys[query][contig]), locs
-
-
-
-def summarize_combined(alns, querys, min_pid, min_aln_len):
-    """
-    simple anchors considers only coverage in query, and makes no attempt to
-    check for reference chaining
-    :param alns:
-    :param querys:
-    :param min_pid:
-    :param min_aln_len:
-    """
-    anchors = load_anchors_combined(alns, min_pid, min_aln_len)
-    for query in anchors.keys():
-        for contig in anchors[query].keys():
-            locs = anchors[query][contig]
-#            print "\t", ref, contig, locs
-            ranges = merge_ranges(locs)
-#            print "\t\t", ranges
-            total_bases = sum(e - s + 1 for s, e in ranges)
-            print "%s\t%s\t%d/%d\t%2.1f" \
-                % (query, contig, total_bases, querys[query][contig],
-                    100 * total_bases / querys[query][contig]), locs
-
-
-def load_anchors_combined(alns, min_pid, min_aln_len):
-    """
-    simple anchoring consider only coverage in query, no reference chaining
-    :param alns:
-    :param min_pid:
-    :param min_aln_len:
-    """
-    anchors = defaultdict(lambda: defaultdict(list))
-    for aln in alns:
-        q_s, q_e = strandless(int(aln.q_s), int(aln.q_e))
-        query_name = aln.query.split("_")[0]
-        if float(aln.pid) >= min_pid and int(aln.q_aln_len) >= min_aln_len:
-            anchors[query_name][aln.query].append([q_s, q_e])
-    return anchors
+                      % (query, ref, contig, total_bases, querys[query],
+                         100 * total_bases / querys[query]), locs
 
 
 def load_anchors_simple(alns, min_pid, min_aln_len):
@@ -157,20 +108,6 @@ def load_anchors_simple(alns, min_pid, min_aln_len):
             anchors[query_name][aln.ref][aln.query].append([q_s, q_e])
     return anchors
 
-
-def load_ref_anchors_simple(alns, min_pid, min_aln_len):
-    """
-    simple anchoring consider only coverage in query, no reference chaining
-    :param alns:
-    :param min_pid:
-    :param min_aln_len:
-    """
-    anchors = defaultdict(list)
-    for aln in alns:
-        r_s, r_e = strandless(int(aln.r_s), int(aln.r_e))
-        if float(aln.pid) >= min_pid and int(aln.r_aln_len) >= min_aln_len:
-            anchors[aln.ref].append([r_s, r_e])
-    return anchors
 
 def strandless(aln_s, aln_e):
     if aln_s < aln_e:
@@ -190,15 +127,15 @@ def main():
                         default=sys.stdout)
     parser.add_argument('input_coord_file', type=str)
     args = parser.parse_args()
-    querys = load_query_assm_lengths(args.query_assm_info_file)
+    querys = load_seq_lengths(args.query_assm_info_file)
     alns = load_coords(args.input_coord_file)
-#    summarize_simple(alns, querys,
-#                     args.min_percent_id, args.min_aln_length)
+    summarize_simple(alns, querys,
+                     args.min_percent_id, args.min_aln_length)
 #    summarize_combined(alns, querys, args.min_percent_id, args.min_aln_length)
 
-    refs = load_ref_assm_lengths(args.query_assm_info_file)
-    summarize_ref_simple(alns, refs,
-                     args.min_percent_id, args.min_aln_length)
+#    refs = load_ref_lengths(args.query_assm_info_file)
+#    summarize_ref_simple(alns, refs,
+#                     args.min_percent_id, args.min_aln_length)
 
 if __name__ == "__main__":
     main()
