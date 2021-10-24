103d65fb110d0f703331bb734ba5a297e79d8a03
ChuShin <chushin.koh@gmail.com>
2021-10-24 15:57:36 -0700

    read data

--- util/call_he.py
+++ util/call_he.py
@@ -4,12 +4,23 @@ import argparse
 import sys
 from collections import namedtuple
 from collections import defaultdict
+from numpy import genfromtxt
 from itertools import chain
 
 
 """call_he.py: Given a coverage file, make HE calls."""
 
 
+def load_covs(filename):
+    covs = []
+#    with open(filename, 'r') as infile:
+#        for line in infile:
+#            dat = line.strip().split(',')
+#            if len(dat) == 13:
+#                alns.append(Alignment._make(dat))
+    covs = genfromtxt(filename, delimiter=',')
+    print covs
+    return covs
 
 
 
@@ -20,15 +31,7 @@ def main():
                         default=sys.stdout)
     parser.add_argument('input_cov_file', type=str)
     args = parser.parse_args()
-    querys = load_query_assm_lengths(args.query_assm_info_file)
     covs = load_covs(args.input_cov_file)
-#    summarize_simple(alns, querys,
-#                     args.min_percent_id, args.min_aln_length)
-#    summarize_combined(alns, querys, args.min_percent_id, args.min_aln_length)
-
-    refs = load_ref_assm_lengths(args.query_assm_info_file)
-    summarize_ref_simple(alns, refs,
-                     args.min_percent_id, args.min_aln_length)
 
 if __name__ == "__main__":
     main()
