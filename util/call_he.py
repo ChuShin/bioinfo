43e19999a79769832659b2698931b76d62107e1a
ChuShin <chushin.koh@gmail.com>
2021-10-24 17:04:01 -0700

    log error

--- util/call_he.py
+++ util/call_he.py
@@ -2,27 +2,36 @@
 
 import argparse
 import sys
+import datetime
 from collections import namedtuple
 from collections import defaultdict
 from numpy import genfromtxt
 from itertools import chain
-
+from datetime import date
 
 """call_he.py: Given a coverage file, make HE calls."""
+
+Coverage = namedtuple('Coverage', 'sample, group, pos, geneA, covA, geneB, covB')
+flatten = chain.from_iterable
 
 
 def load_covs(filename):
     covs = []
-#    with open(filename, 'r') as infile:
-#        for line in infile:
-#            dat = line.strip().split(',')
-#            if len(dat) == 13:
-#                alns.append(Alignment._make(dat))
-    covs = genfromtxt(filename, delimiter=',')
-    print covs
+    with open(filename, 'r') as infile:
+        for line in infile:
+            dat = line.strip().split(',')
+            if len(covs) == 0:
+                if dat[0] != "sample":
+                    log_error("invalid file format.")
+            if len(dat) == 7:
+                covs.append(Coverage._make(dat))
     return covs
 
 
+def log_error(message):
+    dt = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{dt}]: {message}")
+    sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(
