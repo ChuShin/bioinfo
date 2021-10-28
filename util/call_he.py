6975a9bfbb292c47978b7a4f1d707c9b28940d0d
ChuShin <chushin.koh@gmail.com>
2021-10-27 22:50:28 -0700

    report seeded output

--- util/call_he.py
+++ util/call_he.py
@@ -31,8 +31,7 @@ def load_covs(filename):
                 if dat[0] != "sample":
                     log_error(f"invalid file format: {line}")
             elif len(dat) == 7:
-
-                covs[dat[0]][dat[1]].append([float(dat[4]), float(dat[6]), line])
+                covs[dat[0]][dat[1]].append([float(dat[4]), float(dat[6]), line.strip()])
     return covs
 
 def normalize(covs):
@@ -62,7 +61,7 @@ def normalize_sample(sample, sample_cov):
     #disable for now.
     #new_df = df[filtered_entries]
     #print(f"{data}")
-    print(f"{z_scores}")
+    #print(f"{z_scores}")
 
 
 def call_he_events(z_scores, sample_cov):
@@ -73,21 +72,36 @@ def call_he_events(z_scores, sample_cov):
         zscore_B = z_scores[1][i]
         he_type = call_he_type(zscore_A, zscore_B)
         he_array.append(he_type)
-    find_seeds(he_array)
+    seeds = find_seeds(he_array)
+    he_events = extend_seeds(seeds)
+    print_he_output(he_events, z_scores, sample_cov)
+
+
+def print_he_output(he_events, z_scores, sample_cov):
+    for chr_group in sample_cov.keys():
+        for i, gene in enumerate(sample_cov[chr_group]):
+            print(f"{gene[2]},{z_scores[0][i]},{z_scores[1][i]},{he_events[i]}")
+
+
 
 
 def find_seeds(he_array):
     seed_size = 3
     gene_count = len(he_array) - seed_size
-    he_events = [''] * len(he_array)
+    he_seeds = [''] * len(he_array)
     for i, he_type in enumerate(he_array):
         #print(f"{i} {he_type}")
         if i <= gene_count:
             if is_hit(he_array, i, seed_size):
-                print(f"{i} is hit")
+                #print(f"{i} is hit")
                 for j in range(i,i+seed_size):
-                    he_events[j] = he_type
-    print(f"{he_events}")
+                    he_seeds[j] = he_type
+#    print(f"{he_events}")
+    return he_seeds
+
+def extend_seeds(he_array):
+    #skip
+    return he_array
 
 
 def is_hit(he_array, i, seed_size):
